45e15456d05a808c3545d76e8d953caf637d9b44
olgae <olga.e@qualisystems.com>
2016-02-18 17:43:30 +0200

    fix setup.py

--- setup.py
+++ setup.py
@@ -12,14 +12,14 @@ with open('test_requirements.txt') as f_tests:
 
 setup(
     name='cloudshell-networking-cisco',
-    url='https://github.com/QualiSystems/cloudshell-networking-cisco',
+    url='http://www.qualisystems.com/',
     author='QualiSystems',
     author_email='info@qualisystems.com',
-    packages=['cloudshell-networking-cisco'],
+    packages=find_packages(),
     install_requires=required,
-    test_suite='tests',
     tests_require=required_for_tests,
     version=version_from_file,
-    description='QualiSystems Python package',
+    description='CloudShell Networking Cisco Specific package',
     include_package_data = True
 )
+
