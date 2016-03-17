d96aadb393b712c4eeda85bcfb9cbc2a36a43ffd
CoYe <wiseman.kostya@gmail.com>
2016-03-17 11:40:39 +0200

    renamed device from 3650 to 3560

--- cloudshell/networking/cisco/resource_drivers_map.py
+++ cloudshell/networking/cisco/resource_drivers_map.py
@@ -8,5 +8,5 @@ CISCO_RESOURCE_DRIVERS_MAP = \
         '1767': 'CATALYST_3850',
         '1768': 'CATALYST_3850',
         '359': 'CATALYST_2950',
-        '615': 'CATALYST_3650'
+        '615': 'CATALYST_3560'
         }
