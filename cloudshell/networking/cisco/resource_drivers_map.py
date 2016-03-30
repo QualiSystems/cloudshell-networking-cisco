ec6e45bf04a4f6a26eebde78118654391266d202
CoYe <wiseman.kostya@gmail.com>
2016-03-30 10:54:07 +0300

    Fixed bug #26795, removed ASR, ISR

--- cloudshell/networking/cisco/resource_drivers_map.py
+++ cloudshell/networking/cisco/resource_drivers_map.py
@@ -2,8 +2,6 @@ __author__ = 'CoYe'
 
 CISCO_RESOURCE_DRIVERS_MAP = \
         {
-        '923': 'ASR_1K', '924': 'ASR_1K', '925': 'ASR_1K',
-        '805': 'ISR_4K',
         '1410': 'NXOS_5K',
         '1767': 'CATALYST_3850',
         '1768': 'CATALYST_3850',
