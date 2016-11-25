672077e6e2188f92382322aedc2acfa13e8bd373
CoYe <wiseman.kostya@gmail.com>
2016-11-25 17:02:19 +0200

    remove commented code from cisco_configuration_operations

--- cloudshell/networking/cisco/runners/cisco_configuration_operations.py
+++ cloudshell/networking/cisco/runners/cisco_configuration_operations.py
@@ -12,14 +12,3 @@ class CiscoConfigurationRunner(ConfigurationRunner):
                                         logger=self._logger)
         self._restore_flow = CiscoRestoreFlow(cli_handler=self._cli_handler,
                                               logger=self._logger)
-
-    # def save_configuration(self, folder_path, configuration_type=None, vrf_management_name=None):
-    #     """Proxy method for Save command, to modify output according to networking standard
-    #
-    #     :param folder_path:  tftp/ftp server where file be saved
-    #     :param configuration_type: type of configuration that will be saved (StartUp or Running)
-    #     :param vrf_management_name: Virtual Routing and Forwarding management nam
-    #     :return:
-    #     """
-    #     response = CiscoSaveFlow(self._cli_handler)
-    #     return response.identifier.split('/')[-1]
