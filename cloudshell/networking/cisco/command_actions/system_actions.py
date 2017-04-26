38ab60b72894a45cac2be2dd283cbc8d04493469
CoYe <wiseman.kostya@gmail.com>
2017-04-26 14:29:59 +0300

    changes to prepare action map

--- cloudshell/networking/cisco/command_actions/system_actions.py
+++ cloudshell/networking/cisco/command_actions/system_actions.py
@@ -5,6 +5,7 @@ import re
 from collections import OrderedDict
 
 from cloudshell.cli.command_template.command_template_executor import CommandTemplateExecutor
+from cloudshell.devices.networking_utils import UrlParser
 from cloudshell.networking.cisco.command_templates import configuration
 from cloudshell.networking.cisco.command_templates import firmware
 
@@ -26,37 +27,30 @@ class SystemActions(object):
     def prepare_action_map(source_file, destination_file):
         action_map = OrderedDict()
         if "://" in destination_file:
-            destination_file_data_list = re.sub("/+", "/", destination_file).split("/")
-            host = destination_file_data_list[1]
+            url = UrlParser.parse_url(destination_file)
+            dst_file_name = url.get(UrlParser.FILENAME)
             source_file_name = source_file.split(":")[-1].split("/")[-1]
             action_map[r"[\[\(].*{}[\)\]]".format(
-                destination_file_data_list[-1])] = lambda session, logger: session.send_line("", logger)
+                dst_file_name)] = lambda session, logger: session.send_line("", logger)
 
             action_map[r"[\[\(]{}[\)\]]".format(source_file_name)] = lambda session, logger: session.send_line("",
                                                                                                                logger)
         else:
             destination_file_name = destination_file.split(":")[-1].split("/")[-1]
-            source_file_data_list = re.sub("/+", "/", source_file).split("/")
-            host = source_file_data_list[1]
-            source_file_name = source_file_data_list[-1]
+            url = UrlParser.parse_url(source_file)
+
+            source_file_name = url.get(UrlParser.FILENAME)
             action_map[r"(?!/)[\[\(]{}[\)\]]".format(
                 destination_file_name)] = lambda session, logger: session.send_line("", logger)
             action_map[r"(?!/)[\[\(]{}[\)\]]".format(
                 source_file_name)] = lambda session, logger: session.send_line("", logger)
+        host = url.get(UrlParser.HOSTNAME)
         if host:
-            if "@" in host:
-                storage_data = re.search(r"^(?P<user>\S+):(?P<password>\S+)@(?P<host>\S+)", host)
-                if storage_data:
-                    storage_data_dict = storage_data.groupdict()
-                    host = storage_data_dict["host"]
-                    password = storage_data_dict["password"]
-
-                    action_map[r"[Pp]assword:".format(
-                        source_file)] = lambda session, logger: session.send_line(password, logger)
-
-                else:
-                    host = host.split("@")[-1]
             action_map[r"(?!/){}(?!/)".format(host)] = lambda session, logger: session.send_line("", logger)
+        password = url.get(UrlParser.PASSWORD)
+        if password:
+            action_map[r"[Pp]assword:".format(
+                source_file)] = lambda session, logger: session.send_line(password, logger)
         return action_map
 
     def copy(self, source, destination, vrf=None, action_map=None, error_map=None, timeout=120):
