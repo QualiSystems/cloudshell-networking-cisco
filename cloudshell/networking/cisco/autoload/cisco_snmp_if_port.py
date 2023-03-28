96f167cd5c9d990b79164f7af10db6841626eb8d
Costya <costya.y@qualisystems.com>
2023-03-28 12:10:04 -0500

    Updating Port names match and add replace unsupported chrs

--- cloudshell/networking/cisco/autoload/cisco_snmp_if_port.py
+++ cloudshell/networking/cisco/autoload/cisco_snmp_if_port.py
@@ -5,8 +5,7 @@ from cloudshell.snmp.autoload.snmp.helper.snmp_if_entity import SnmpIfEntity
 
 class CiscoSnmpIfPort(SnmpIfEntity):
     PORT_NAME_MATCH = re.compile(
-        r"^(GigabitEthernet|FastEthernet|FortyGig|"
-        r"TenGig|Hundred|Ethernet|Port-channel)",
+        r"\S*(Ethernet|GigE|Serial|Sonet|Port-channel|Bundle-Ether)",
         re.IGNORECASE,
     )
 
@@ -15,4 +14,4 @@ class CiscoSnmpIfPort(SnmpIfEntity):
         name = self.if_descr_name
         if not self.PORT_NAME_MATCH.search(name):
             name = self.if_name
-        return name
+        return name.replace("/", "-").replace(":", "_")
