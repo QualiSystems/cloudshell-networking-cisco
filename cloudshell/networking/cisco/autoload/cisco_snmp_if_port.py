77b1d340fb59f317585752139387b0694e3efdd3
Costya <costya.y@qualisystems.com>
2023-03-29 09:44:54 -0500

    Added POS ports

--- cloudshell/networking/cisco/autoload/cisco_snmp_if_port.py
+++ cloudshell/networking/cisco/autoload/cisco_snmp_if_port.py
@@ -5,7 +5,7 @@ from cloudshell.snmp.autoload.snmp.helper.snmp_if_entity import SnmpIfEntity
 
 class CiscoSnmpIfPort(SnmpIfEntity):
     PORT_NAME_MATCH = re.compile(
-        r"\S*(Ethernet|GigE|Serial|Sonet|Port-channel|Bundle-Ether)",
+        r"\S*(Ethernet|GigE|Serial|Sonet|POS|Port-channel|Bundle-Ether)",
         re.IGNORECASE,
     )
 
